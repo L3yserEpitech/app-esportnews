13d9b37e556924d0b86fdce7b9983e85226c55af
Jules Lordet <146731380+L3yserEpitech@users.noreply.github.com>
2025-09-22 23:00:57 +0200

    Delete esport_news directory

--- esport_news/android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package esportnews.fr
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- esport_news/lib/app_constants.dart
+++
@@ -1,6 +0,0 @@
-
-abstract class FFAppConstants {
-  static const String errorLogin = 'ERROR_CODE_ACCESS_DENIED';
-  static const String cs = 'CS:GO';
-  static const String imageExternal = 'image/external';
-}
