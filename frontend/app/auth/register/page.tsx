c043c7d342e50d0112edc7bf13eea6e456cec56a
Jules <jules.lordet@epitech.eu>
2025-11-08 22:28:19 +0100

    ♻️ Refacto: modification page login/register

--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -120,7 +120,6 @@ export default function RegisterPage() {
       {/* Background effects */}
       <div className="absolute inset-0 overflow-hidden">
         <div className="absolute -top-40 -right-40 w-80 h-80 bg-[#F22E62]/20 rounded-full blur-3xl"></div>
-        <div className="absolute -bottom-40 -left-40 w-80 h-80 bg-[#182859]/40 rounded-full blur-3xl"></div>
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-96 h-96 bg-[#F22E62]/10 rounded-full blur-3xl"></div>
       </div>
 
